75e94044794d52a8cd3aca08fc331c0a2c25cc5b
Hans Tanawi <hans.tanawi@outlook.com>
2023-10-31 22:18:39 +0700

    chore: add config paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Button from 'components/ui/button';
+
 function App() {
   return (
     <div>
       <h1 className="text-red-500">Hello world</h1>
+      <Button>Hello</Button>
     </div>
   );
 }
