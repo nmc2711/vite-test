1b056bb3316d238b2a9e6f7207e521b5f76e1fe2
Hans Tanawi <hans.tanawi@outlook.com>
2023-10-31 17:15:02 +0700

    feat: add lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit',
+
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `pnpm eslint ${filenames.join(' ')} --fix`,
+    `pnpm format`,
+  ],
+
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': (filenames) =>
+    `pnpm prettier --write ${filenames.join(' ')}`,
+};
