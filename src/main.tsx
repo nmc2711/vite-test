5e0b7689eb0c9374c934672cc4cd96dd83da92b1
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-01 16:59:56 +0700

    feat: add starting pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,35 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import AboutPage from '@/pages/about';
+import Homepage from '@/pages/home';
+import NotFoundPage from '@/pages/not-found';
 
 import App from './App';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: '',
+        element: <Homepage />,
+        index: true,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
