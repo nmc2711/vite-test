8c21b07c5320f04a4c75ab53c524495113f5952e
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-01 17:00:15 +0700

    feat: app layout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Button from '@/components/ui/button';
+import { Outlet } from 'react-router-dom';
+
+import Navbar from '@/components/navbar';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500">Hello world</h1>
-      <Button>Hello</Button>
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <header>
+      <div>
+        <nav />
+      </div>
+    </header>
+  );
+}
