d0e2fbd0dd79fa0898a6fcc7dcbd279cca90de94
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-04 22:09:06 +0700

    style: update error page styling

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,7 +1,19 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
 export default function NotFoundPage() {
+  const error = useRouteError() as Error;
+
+  if (!isRouteErrorResponse(error)) {
+    return null;
+  }
+
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="flex w-full flex-col items-center justify-center gap-y-4">
+      <h1 className="text-5xl font-bold">Oops!</h1>
+      <p className="text-xl">Sorry, an unexpected error has occurred.</p>
+      <p className="text-lg text-gray-500">
+        <i>{error.statusText || error.message}</i>
+      </p>
     </div>
   );
 }
