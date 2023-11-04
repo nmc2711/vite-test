34c4002ec6583d091b1e1cced8ebb83b5624f734
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-04 23:09:00 +0700

    feat: add navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,24 @@
+import { NavLink } from 'react-router-dom';
+
 export default function Navbar() {
   return (
-    <header>
-      <div>
-        <nav />
+    <header className="fixed h-6 w-full p-10">
+      <div className="flex h-full items-center justify-between">
+        <h1 className="text-xl font-semibold">React + TypeScript Starter</h1>
+        <nav>
+          <ul className="flex gap-x-6">
+            <li>
+              <NavLink to="/" className="text-black dark:text-white">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/learn-more" className="text-black dark:text-white">
+                Learn More
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
