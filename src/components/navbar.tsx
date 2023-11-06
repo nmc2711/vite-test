bf6b05962d632fa796e8dcc1792968c7975dd1f4
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-06 14:16:14 +0700

    style: navbar styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,32 @@
 import { NavLink } from 'react-router-dom';
 
+function StyledNavLink({ href, title }: { href: string; title: string }) {
+  return (
+    <NavLink
+      to={href}
+      className={({ isActive }) =>
+        isActive
+          ? 'rounded-2xl bg-primary bg-opacity-10 px-4 py-1.5 text-sm'
+          : 'px-4 py-1.5 text-sm font-medium hover:text-primary'
+      }
+    >
+      {title}
+    </NavLink>
+  );
+}
+
 export default function Navbar() {
   return (
     <header className="fixed h-6 w-full p-10">
       <div className="flex h-full items-center justify-between">
-        <h1 className="text-xl font-semibold">React + TypeScript Starter</h1>
+        <h1 className="text-lg font-semibold">React + TypeScript Starter</h1>
         <nav>
           <ul className="flex gap-x-6">
             <li>
-              <NavLink to="/" className="text-black dark:text-white">
-                Home
-              </NavLink>
+              <StyledNavLink href="/" title="Home" />
             </li>
             <li>
-              <NavLink to="/learn-more" className="text-black dark:text-white">
-                Learn More
-              </NavLink>
+              <StyledNavLink href="learn-more" title="Learn More" />
             </li>
           </ul>
         </nav>
