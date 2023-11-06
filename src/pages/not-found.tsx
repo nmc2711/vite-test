07c1f426230d41a9f95ac75d81fd01329f5753ce
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-06 14:15:32 +0700

    fix: not found page styling

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,4 +1,6 @@
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+
+import Button from '@/components/ui/button';
 
 export default function NotFoundPage() {
   const error = useRouteError() as Error;
@@ -8,12 +10,15 @@ export default function NotFoundPage() {
   }
 
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-y-4">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-y-4">
       <h1 className="text-5xl font-bold">Oops!</h1>
       <p className="text-xl">Sorry, an unexpected error has occurred.</p>
       <p className="text-lg text-gray-500">
         <i>{error.statusText || error.message}</i>
       </p>
+      <Button>
+        <Link to="/">Go to Homepage</Link>
+      </Button>
     </div>
   );
 }
