af63c42c962eda49a19119bfc5d53207921884c2
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-11 22:30:38 +0800

    test: learn more page unit test

---
+++ src/pages/learn-more.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen, within } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import LearnMorePage from './learn-more';
+
+function renderComponent() {
+  render(
+    <MemoryRouter>
+      <LearnMorePage />
+    </MemoryRouter>,
+  );
+}
+
+describe('learn-more.tsx', () => {
+  it('should render page title', () => {
+    renderComponent();
+
+    const pageTitle = screen.getByRole('heading', {
+      level: 1,
+    });
+
+    expect(pageTitle).toHaveTextContent("What's inside?");
+  });
+
+  it('should render list of technologies list item', () => {
+    renderComponent();
+
+    const list = screen.getByRole('list');
+    const listItems = screen.getAllByRole('listitem');
+
+    expect(list).toBeInTheDocument();
+    expect(listItems).toHaveLength(13);
+  });
+
+  it('should render a learn more button with a link', () => {
+    renderComponent();
+
+    const button = screen.getByRole('button');
+    const link = within(button).getByRole('link');
+
+    expect(button).toBeInTheDocument();
+    expect(link).toHaveAttribute(
+      'href',
+      'https://github.com/hanstanawi/react-ts-starter-template',
+    );
+  });
+});
